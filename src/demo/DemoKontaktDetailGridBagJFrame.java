b5415056715cc3b38867e6797e57b86ceed3016a
Benjamin Mahr <ben.mahr@gmail.com>
2014-05-06 21:25:34 +0200

    GridBagLayout done

--- src/demo/DemoKontaktDetailGridBagJFrame.java
+++ src/demo/DemoKontaktDetailGridBagJFrame.java
@@ -20,6 +20,9 @@ import java.awt.event.FocusListener;
 import javax.swing.SwingConstants;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 
 public class DemoKontaktDetailGridBagJFrame {
@@ -96,74 +99,75 @@ public class DemoKontaktDetailGridBagJFrame {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 326, 398);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		
 		FocusListener saveCecker =  new CheckSaveableFocusListener();
 			
 		ImageIcon errorIcon = new ImageIcon(DemoKontaktDetailGridBagJFrame.class.getResource("/com/sun/java/swing/plaf/windows/icons/Error.gif"));
-		
-		JLabel lblKontaktDetails = new JLabel("Kontakt Details");
-		lblKontaktDetails.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblKontaktDetails.setBounds(10, 15, 129, 17);
-		frame.getContentPane().add(lblKontaktDetails);
-		
-		JLabel lblName = new JLabel("Name:");
-		lblName.setBounds(10, 47, 71, 14);
-		frame.getContentPane().add(lblName);
-		
-		nameField = new JTextField();
-		nameField.addFocusListener(saveCecker);
-		nameField.setBounds(97, 44, 150, 20);
-		frame.getContentPane().add(nameField);
-		nameField.setColumns(10);
-
-		namesErrorLabel = new JLabel(errorIcon);
-		namesErrorLabel.setVisible(false);
-		namesErrorLabel.setToolTipText("Vorname und Nachmame duerfen nicht beide leer sein");
-		namesErrorLabel.setBounds(265, 47, 32, 32);
-		frame.getContentPane().add(namesErrorLabel);
-	
-		JTextArea notesArea = new JTextArea();
-		notesArea.setBounds(10, 193, 220, 119);
-		frame.getContentPane().add(notesArea);
-		
-		saveButton = new JButton("Save");
-		saveButton.setEnabled(false);
-		saveButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("Saved record for: "+getNameField().getText()+" "+getFirstNameField().getText());
-			}
-		});
-		saveButton.setBounds(10, 323, 89, 23);
-		frame.getContentPane().add(saveButton);
-		
-		JLabel lblNotizen = new JLabel("Notizen:");
-		lblNotizen.setBounds(10, 177, 71, 14);
-		frame.getContentPane().add(lblNotizen);
-		
-		JLabel lblNewLabel = new JLabel("Vorname:");
-		lblNewLabel.setBounds(10, 73, 70, 15);
-		frame.getContentPane().add(lblNewLabel);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{71, 151, 32, 0};
+		gridBagLayout.rowHeights = new int[]{17, 20, 19, 34, 32, 14, 119, 23, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
 		
 		vornameField = new JTextField();
 		vornameField.addFocusListener(saveCecker);
-		vornameField.setBounds(97, 71, 150, 19);
-		frame.getContentPane().add(vornameField);
+		
+				namesErrorLabel = new JLabel(errorIcon);
+				namesErrorLabel.setVisible(false);
+				
+				JLabel lblKontaktDetails = new JLabel("Kontakt Details");
+				lblKontaktDetails.setFont(new Font("Tahoma", Font.PLAIN, 14));
+				GridBagConstraints gbc_lblKontaktDetails = new GridBagConstraints();
+				gbc_lblKontaktDetails.anchor = GridBagConstraints.NORTHWEST;
+				gbc_lblKontaktDetails.insets = new Insets(0, 0, 5, 5);
+				gbc_lblKontaktDetails.gridwidth = 2;
+				gbc_lblKontaktDetails.gridx = 0;
+				gbc_lblKontaktDetails.gridy = 0;
+				frame.getContentPane().add(lblKontaktDetails, gbc_lblKontaktDetails);
+				
+				JLabel lblName = new JLabel("Name:");
+				GridBagConstraints gbc_lblName = new GridBagConstraints();
+				gbc_lblName.fill = GridBagConstraints.HORIZONTAL;
+				gbc_lblName.insets = new Insets(0, 0, 5, 5);
+				gbc_lblName.gridx = 0;
+				gbc_lblName.gridy = 1;
+				frame.getContentPane().add(lblName, gbc_lblName);
+				
+				nameField = new JTextField();
+				nameField.addFocusListener(saveCecker);
+				GridBagConstraints gbc_nameField = new GridBagConstraints();
+				gbc_nameField.fill = GridBagConstraints.BOTH;
+				gbc_nameField.insets = new Insets(0, 0, 5, 5);
+				gbc_nameField.gridx = 1;
+				gbc_nameField.gridy = 1;
+				frame.getContentPane().add(nameField, gbc_nameField);
+				nameField.setColumns(10);
+				namesErrorLabel.setToolTipText("Vorname und Nachmame duerfen nicht beide leer sein");
+				GridBagConstraints gbc_namesErrorLabel = new GridBagConstraints();
+				gbc_namesErrorLabel.anchor = GridBagConstraints.NORTH;
+				gbc_namesErrorLabel.fill = GridBagConstraints.HORIZONTAL;
+				gbc_namesErrorLabel.insets = new Insets(0, 0, 5, 0);
+				gbc_namesErrorLabel.gridheight = 2;
+				gbc_namesErrorLabel.gridx = 2;
+				gbc_namesErrorLabel.gridy = 1;
+				frame.getContentPane().add(namesErrorLabel, gbc_namesErrorLabel);
+		
+		JLabel lblNewLabel = new JLabel("Vorname:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 2;
+		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		GridBagConstraints gbc_vornameField = new GridBagConstraints();
+		gbc_vornameField.anchor = GridBagConstraints.NORTH;
+		gbc_vornameField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_vornameField.insets = new Insets(0, 0, 5, 5);
+		gbc_vornameField.gridx = 1;
+		gbc_vornameField.gridy = 2;
+		frame.getContentPane().add(vornameField, gbc_vornameField);
 		vornameField.setColumns(10);
-		
-		JLabel lblEmail = new JLabel("eMail:");
-		lblEmail.setBounds(10, 108, 70, 15);
-		frame.getContentPane().add(lblEmail);
-		
-		JLabel lblTelefon = new JLabel("Telefon:");
-		lblTelefon.setBounds(10, 150, 70, 15);
-		frame.getContentPane().add(lblTelefon);
-		
-		emailField = new JTextField();
-		emailField.addFocusListener(saveCecker);
-		emailField.setBounds(97, 106, 150, 19);
-		frame.getContentPane().add(emailField);
-		emailField.setColumns(10);
 		
 		telfonField = new JTextField();
 		telfonField.addKeyListener(new KeyAdapter() {
@@ -173,25 +177,102 @@ public class DemoKontaktDetailGridBagJFrame {
 			}
 		});
 		telfonField.addFocusListener(saveCecker);
-		telfonField.setBounds(96, 146, 150, 19);
-		frame.getContentPane().add(telfonField);
+		
+		JLabel lblEmail = new JLabel("eMail:");
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.anchor = GridBagConstraints.SOUTH;
+		gbc_lblEmail.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEmail.gridx = 0;
+		gbc_lblEmail.gridy = 3;
+		frame.getContentPane().add(lblEmail, gbc_lblEmail);
+		
+		mailError = new JLabel("");
+		mailError.setVisible(false);
+		
+		emailField = new JTextField();
+		emailField.addFocusListener(saveCecker);
+		GridBagConstraints gbc_emailField = new GridBagConstraints();
+		gbc_emailField.anchor = GridBagConstraints.SOUTH;
+		gbc_emailField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_emailField.insets = new Insets(0, 0, 5, 5);
+		gbc_emailField.gridx = 1;
+		gbc_emailField.gridy = 3;
+		frame.getContentPane().add(emailField, gbc_emailField);
+		emailField.setColumns(10);
+		mailError.setHorizontalAlignment(SwingConstants.CENTER);
+		mailError.setToolTipText("eMail darf nicht leer sein");
+		mailError.setIcon(new ImageIcon(DemoKontaktDetailGridBagJFrame.class.getResource("/com/sun/java/swing/plaf/windows/icons/Error.gif")));
+		GridBagConstraints gbc_mailError = new GridBagConstraints();
+		gbc_mailError.anchor = GridBagConstraints.NORTH;
+		gbc_mailError.fill = GridBagConstraints.HORIZONTAL;
+		gbc_mailError.insets = new Insets(0, 0, 5, 0);
+		gbc_mailError.gridx = 2;
+		gbc_mailError.gridy = 3;
+		frame.getContentPane().add(mailError, gbc_mailError);
+		
+		JLabel lblTelefon = new JLabel("Telefon:");
+		GridBagConstraints gbc_lblTelefon = new GridBagConstraints();
+		gbc_lblTelefon.anchor = GridBagConstraints.SOUTH;
+		gbc_lblTelefon.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblTelefon.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTelefon.gridx = 0;
+		gbc_lblTelefon.gridy = 4;
+		frame.getContentPane().add(lblTelefon, gbc_lblTelefon);
+		GridBagConstraints gbc_telfonField = new GridBagConstraints();
+		gbc_telfonField.anchor = GridBagConstraints.SOUTH;
+		gbc_telfonField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_telfonField.insets = new Insets(0, 0, 5, 5);
+		gbc_telfonField.gridx = 1;
+		gbc_telfonField.gridy = 4;
+		frame.getContentPane().add(telfonField, gbc_telfonField);
 		telfonField.setColumns(10);
+		
+		saveButton = new JButton("Save");
+		saveButton.setEnabled(false);
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Saved record for: "+getNameField().getText()+" "+getFirstNameField().getText());
+			}
+		});
 		
 		telefonError = new JLabel("");
 		telefonError.setVisible(false);
 		telefonError.setHorizontalAlignment(SwingConstants.CENTER);
 		telefonError.setToolTipText("Telefon darf nicht leer sein");
 		telefonError.setIcon(new ImageIcon(DemoKontaktDetailGridBagJFrame.class.getResource("/com/sun/java/swing/plaf/windows/icons/Error.gif")));
-		telefonError.setBounds(265, 133, 32, 32);
-		frame.getContentPane().add(telefonError);
+		GridBagConstraints gbc_telefonError = new GridBagConstraints();
+		gbc_telefonError.anchor = GridBagConstraints.NORTH;
+		gbc_telefonError.fill = GridBagConstraints.HORIZONTAL;
+		gbc_telefonError.insets = new Insets(0, 0, 5, 0);
+		gbc_telefonError.gridx = 2;
+		gbc_telefonError.gridy = 4;
+		frame.getContentPane().add(telefonError, gbc_telefonError);
 		
-		mailError = new JLabel("");
-		mailError.setVisible(false);
-		mailError.setHorizontalAlignment(SwingConstants.CENTER);
-		mailError.setToolTipText("eMail darf nicht leer sein");
-		mailError.setIcon(new ImageIcon(DemoKontaktDetailGridBagJFrame.class.getResource("/com/sun/java/swing/plaf/windows/icons/Error.gif")));
-		mailError.setBounds(265, 91, 32, 32);
-		frame.getContentPane().add(mailError);
+		JLabel lblNotizen = new JLabel("Notizen:");
+		GridBagConstraints gbc_lblNotizen = new GridBagConstraints();
+		gbc_lblNotizen.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblNotizen.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNotizen.gridx = 0;
+		gbc_lblNotizen.gridy = 5;
+		frame.getContentPane().add(lblNotizen, gbc_lblNotizen);
+		
+			JTextArea notesArea = new JTextArea();
+			GridBagConstraints gbc_notesArea = new GridBagConstraints();
+			gbc_notesArea.fill = GridBagConstraints.BOTH;
+			gbc_notesArea.insets = new Insets(0, 0, 5, 5);
+			gbc_notesArea.gridwidth = 2;
+			gbc_notesArea.gridx = 0;
+			gbc_notesArea.gridy = 6;
+			frame.getContentPane().add(notesArea, gbc_notesArea);
+		GridBagConstraints gbc_saveButton = new GridBagConstraints();
+		gbc_saveButton.anchor = GridBagConstraints.WEST;
+		gbc_saveButton.fill = GridBagConstraints.VERTICAL;
+		gbc_saveButton.insets = new Insets(0, 0, 0, 5);
+		gbc_saveButton.gridwidth = 2;
+		gbc_saveButton.gridx = 0;
+		gbc_saveButton.gridy = 7;
+		frame.getContentPane().add(saveButton, gbc_saveButton);
 		
 	}
 	
