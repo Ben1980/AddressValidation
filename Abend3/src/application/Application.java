2afe295a4a8d53c4c995562814361680f5d96c50
rzaghet <raffael.zaghet@gmail.com>
2014-05-24 10:25:41 +0200

    adding some data samples for contactstore (for testing)

--- Abend3/src/application/Application.java
+++ Abend3/src/application/Application.java
@@ -2,6 +2,8 @@ package application;
 
 import javax.swing.SwingUtilities;
 
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
+
 import domain.Contact;
 import domain.ContactStore;
 import view.ContactListMasterDetailFrame;
@@ -13,7 +15,9 @@ public class Application {
 			public void run() {
 				try {
 					ContactStore contactStore = new ContactStore();
-					
+					//sample contact
+					addSampleDataToStore(contactStore);
+										
 					ContactListMasterDetailFrame window = new ContactListMasterDetailFrame(contactStore);
 					
 					ContactListMasterDetailFrame window2 = new ContactListMasterDetailFrame(contactStore);
@@ -23,6 +27,21 @@ public class Application {
 			}
 		});
 
+		
 	}
-
+	
+	private static void addSampleDataToStore(ContactStore contactStore){
+		addSampleContact(contactStore, "Ghostreiter", "Robin", "robin.ghostreiter@test.ch", "007007007");
+		addSampleContact(contactStore, "Master", "Ben", "ben.master@test.ch", "008008008");
+		addSampleContact(contactStore, "Slave", "Raffi", "raffi.slave@test.ch", "009009009");
+		
+	}
+	
+	private static void addSampleContact(ContactStore contactStore, String lastName, String firstName, String email, String tel){
+		Contact newContact = contactStore.getNewContact();
+		newContact.setFirstName(firstName);
+		newContact.setLastName(lastName);
+		newContact.setEmail(email);
+		newContact.setTelNr(tel);
+	}
 }
